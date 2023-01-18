6df69016bd39d30aafd9a56dea6b87ab1e52076e
Casey Woodman <caseytwoodman@gmail.com>
2023-01-18 17:45:46 -0600

    added Notes HTML Route

--- server.js
+++ server.js
@@ -12,5 +12,6 @@ app.use(express.static("public"));
 
 // GET Route for homepage
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
 
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`));
