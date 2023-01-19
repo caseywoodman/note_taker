9d914b61169e0fe86ca61aecc086f96c210f4b39
Casey Woodman <caseytwoodman@gmail.com>
2023-01-18 18:20:59 -0600

    connected post route for notes and tested

--- routes/notes.js
+++ routes/notes.js
@@ -1,31 +1,30 @@
 const notes = require("express").Router();
-// const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-// const { v4: uuidv4 } = require('uuid');
+const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
+const { v4: uuidv4 } = require("uuid");
 
 // GET Route for retrieving all the notes
 notes.get("/", (req, res) => {
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
-// POST Route for a new UX/UI tip
-// notes.post('/', (req, res) => {
-//   console.log(req.body);
+// POST Route for a new note
+notes.post("/", (req, res) => {
+  console.log(req.body);
 
-//   const { username, topic, tip } = req.body;
+  const { title, text } = req.body;
 
-//   if (req.body) {
-//     const newTip = {
-//       username,
-//       tip,
-//       topic,
-//       tip_id: uuidv4(),
-//     };
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
 
-//     readAndAppend(newTip, './db/notes.json');
-//     res.json(`Tip added successfully 🚀`);
-//   } else {
-//     res.error('Error in adding tip');
-//   }
-// });
+    readAndAppend(newNote, "./db/db.json");
+    res.json(`Note Added Successfully 🚀`);
+  } else {
+    res.error("Note Add Failed");
+  }
+});
 
 module.exports = notes;
